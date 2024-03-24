245e87a3ac57b0fd5e26acb54ecad0cab776de95
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-23 18:50:33 -0700

    next auth

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "./db";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error("No credentials provided");
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    session({ session, user }: any) {
+      // not needed but fixing a temp issue in nextauth
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
