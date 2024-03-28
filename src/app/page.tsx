cfe2f9197831aaab0e4751b6a8bc6342ea36e4fe
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-28 16:30:22 -0700

    topics list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import TopicCreateForm from "@/components/topics/TopicCreatForm";
 import TopicsList from "@/components/topics/TopicsList";
+import { Divider } from "@nextui-org/react";
 
 export default async function Home() {
   return (
@@ -11,8 +12,10 @@ export default async function Home() {
           <li>Post 2</li>
         </ul>
       </div>
-      <div>
+      <div className='border shadow py-3 px-2'>
         <TopicCreateForm />
+        <Divider className='my-2' />
+        <h3 className='text-lg'>Topics</h3>
         <TopicsList />
       </div>
     </div>
--- src/components/topics/TopicsList.tsx
+++ src/components/topics/TopicsList.tsx
@@ -1,7 +1,37 @@
+import { getTopics } from "@/actions/topics/get-topics";
+import { paths } from "@/helpers/paths";
+import { Chip } from "@nextui-org/react";
+import Link from "next/link";
 import React from "react";
 
-const TopicsList = () => {
-  return <div>TopicsList</div>;
+const TopicsList = async () => {
+  const { topics, error } = await getTopics();
+  console.log("list", topics);
+  if (error?.general) {
+    return <p>{error.general}</p>;
+  }
+
+  return (
+    <div>
+      {topics ? (
+        <ul className='flex flex-row flex-wrap gap-2'>
+          {topics.map((topic) => {
+            return (
+              <li className='underline' key={topic.id}>
+                <Link href={paths.topicShow(topic.slug)}>
+                  <Chip variant='shadow' color='warning'>
+                    {topic.slug}
+                  </Chip>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>No Topics available</p>
+      )}
+    </div>
+  );
 };
 
 export default TopicsList;
