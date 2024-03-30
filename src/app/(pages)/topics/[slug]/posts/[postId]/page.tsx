20b29a82e066acb9f656140b348e4442967daf73
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-29 17:28:26 -0700

    post queries

--- src/app/(pages)/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/(pages)/topics/[slug]/posts/[postId]/page.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import { paths } from "@/helpers/paths";
 
-const ShowPostPage = ({
-  params: { postId, slug },
-}: {
-  params: { postId: string; slug: string };
-}) => {
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({
+  params,
+}: PostShowPageProps) {
+  const { slug, postId } = params;
+
   return (
-    <div>
-      Show Post Page
-      <h1>{slug}</h1>
-      <p>Post Id: {postId}</p>
+    <div className='space-y-3'>
+      <Link
+        className='underline decoration-solid'
+        href={paths.topicShow(slug)}
+      >
+        {"< "}Back to {slug}
+      </Link>
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
     </div>
   );
-};
-
-export default ShowPostPage;
+}
---
+++ src/db/queries/posts.ts
@@ -0,0 +1,27 @@
+import type { Post, PrismaPromise } from "@prisma/client";
+import db from "..";
+
+type PostWithData = Post & {
+  topic: {
+    slug: string;
+  };
+  user: { name: string | null };
+  _count: {
+    comments: number;
+  };
+};
+
+export function fetchPostByTopicSlug(
+  slug: string
+): PrismaPromise<PostWithData[]> {
+  return db.post.findMany({
+    where: {
+      topic: { slug },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
