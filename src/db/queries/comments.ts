3ea5d635bebea37e425d4121e528bd129aafd1ee
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-03-31 19:39:00 -0700

    fetchCommentsByPostId

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,17 +1,18 @@
 import type { Comment } from "@prisma/client";
 import db from "..";
 
-export type CommentWithUser = Comment & {
+export type CommentWithAuther = Comment & {
   user: {
     name: string | null;
     image: string | null;
   };
 };
 
-export const fetchComments = async (): Promise<
-  CommentWithUser[] | null
-> => {
+export const fetchCommentsByPostId = async (
+  postId: string
+): Promise<CommentWithAuther[]> => {
   const comments = await db.comment.findMany({
+    where: { postId },
     include: {
       user: { select: { name: true, image: true } },
     },
