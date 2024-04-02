c5614dc306d64322f621d9ac8d811cb3abc71529
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-01 18:37:16 -0700

    post skeleton alt

---
+++ src/components/posts/post-skeleton-alt.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostSkeletionAlt = () => {
+  return (
+    <div className='m-4 animate-pulse'>
+      <h1 className='h-8 bg-gray-200 rounded dark:bg-gray-700 mb-2.5 max-w-xl'></h1>
+      <p className='h-[57.6px] bg-gray-200 rounded dark:bg-gray-700 mb-2.5'></p>
+    </div>
+  );
+};
+
+export default PostSkeletionAlt;
