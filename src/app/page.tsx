07d09f7ce5258842b5979b4d9ee8b936b43d96d5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-01 18:53:10 -0700

    home page post list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
+import PostList from "@/components/posts/post-list";
 import TopicCreateForm from "@/components/topics/TopicCreatForm";
 import TopicsList from "@/components/topics/TopicsList";
+import { fetchTopPostsByComments } from "@/db/queries/posts";
 import { Divider } from "@nextui-org/react";
 
 export default async function Home() {
   return (
     <div className='grid grid-cols-4 gap-4 p-4'>
-      <div className='col-span-3 bg-red-300'>
-        <h1>Posts</h1>
-        <ul>
-          <li>Post 1</li>
-          <li>Post 2</li>
-        </ul>
+      <div className='col-span-3'>
+        <h1 className='text-xl m-2'>Top Posts</h1>
+        <PostList fetchData={fetchTopPostsByComments} />
       </div>
       <div className='border shadow py-3 px-2'>
         <TopicCreateForm />
