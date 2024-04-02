c318c35899bb0ed727d2bca86ee2c21dee4355a9
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-02 16:54:05 -0700

    search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Input,
 } from "@nextui-org/react";
 
 import { paths } from "@/helpers/paths";
 import HeaderAuth from "./HeaderAuth";
 import { Suspense } from "react";
+import SearchInput from "./SearchInput";
 
 const Header = () => {
   return (
@@ -21,7 +21,7 @@ const Header = () => {
       </NavbarBrand>
       <NavbarContent justify='center'>
         <NavbarItem>
-          <Input type='search' />
+          <SearchInput />
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify='end'>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { search } from "@/actions/search";
 import { Input } from "@nextui-org/react";
+import { useSearchParams } from "next/navigation";
 
 const SearchInput = () => {
-  return <Input />;
+  const searchParams = useSearchParams();
+
+  return (
+    <form action={search}>
+      <Input
+        type='text'
+        name='term'
+        defaultValue={searchParams.get("term") || ""}
+      />
+    </form>
+  );
 };
 
 export default SearchInput;
